d8f1613462ebaf04014baba09df41bb7a3bbc12e
IuriiSky <uzik.fk11@gmail.com>
2019-03-07 20:55:38 +0200

    make footer

--- ibp/src/app/app-routing.module.ts
+++ ibp/src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BoardsComponent } from './header/boards/boards.component';
 import { HomeComponent } from './header/home/home.component';
+import { RulesComponent } from './footer/rules/rules.component';
+import { ContactsComponent } from './footer/contacts/contacts.component';
+import { AboutComponent } from './footer/about/about.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'boards', component: BoardsComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'rules', component: RulesComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
---
+++ ibp/src/app/footer/contacts/contacts.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.scss']
+})
+export class ContactsComponent implements OnInit {
+
+  fullContactsControl: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.fullContactsControl = new FormGroup({
+      name: new FormControl('',[Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      text: new FormControl('', [Validators.required, Validators.maxLength(1000)])
+      
+    });
+
+    this.fullContactsControl.valueChanges.subscribe((value) => console.log(value));
+    this.fullContactsControl.statusChanges.subscribe((status) => console.log(status));
+  }
+
+}
