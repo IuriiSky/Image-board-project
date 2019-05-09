79a011090d3036c94341badc6360d97658891ea2
IuriiSky <uzik.fk11@gmail.com>
2019-05-09 18:34:30 +0300

    change app.component

--- ibp/src/app/app-routing.module.ts
+++ ibp/src/app/app-routing.module.ts
@@ -13,9 +13,10 @@ import { NotauthGuard } from './guards/notauth.guard';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PostsComponent } from './posts/posts.component';
 import { BoardDetailsComponent } from './header/board-details/board-details.component';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/about', pathMatch: 'full'},
   {path: 'boards', component: BoardsComponent},
   {path: 'boards/:short_name', component: BoardDetailsComponent},
   {path: 'boards/:board_index/createpost', component: PostsComponent},
@@ -25,7 +26,7 @@ const routes: Routes = [
   {path: 'contacts', component: ContactsComponent},
   {path: 'about', component: AboutComponent},
   {path: 'me', component: CabinetComponent, canActivate: [AuthorizationGuard]},
-  {path: '**', component: PageNotFoundComponent},
+  // {path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
