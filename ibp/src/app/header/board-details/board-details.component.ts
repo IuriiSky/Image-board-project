3809f6404f07c574580db9907f0adc46cb53d0f6
IuriiSky <uzik.fk11@gmail.com>
2019-05-11 21:43:20 +0300

    upgrade post

--- ibp/src/app/header/board-details/board-details.component.ts
+++ ibp/src/app/header/board-details/board-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BoardDetail, PostDetails, PostAttachment } from '../../shared/interfaces/board-detail.interfaces';
+import { BoardDetail, PostDetails, ImagesAttachment } from '../../shared/interfaces/board-detail.interfaces';
 import { BoardDetailService } from '../../services/board-detail.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
@@ -38,46 +38,11 @@ export class BoardDetailsComponent implements OnInit {
   }
 
   getImageAttachment(post: PostDetails){
-    let attachments = post.content.attachments.filter(a => a.type === "Image").map(a => a.file);
+    let attachments = post.content.images.map(a => a.file);
     return attachments;
   }
 
   addBaseUrl(source: string){
     return this.baseUrl + source;
   };
-  
-  // getImageUrl(post: PostDetails) {
-  //   //let imageAttachment = this.getAttachment("Image", post.content.attachments);
-  //   let imageAttachment = this.getImageAttachment(post);
-  //   let url = imageAttachment[0].file.url;
-  //   return this.baseUrl + url;
-  // };
-  
-
-  // getThumbImageUrl(post: PostDetails) {
-  //   //let imageAttachment = this.getAttachment("Image", post.content.attachments);
-  //   //let imageAttachment = this.getImageAttachment(post.content.attachments);
-  //   let imageAttachments = this.getImageAttachment(post);
-  //   let result = imageAttachments.map(a => this.baseUrl + a.file.thumb.url);
-  //   return result;
-
-  //   // let arr = new Array();
-  //   // imageAttachments.forEach(a => {
-  //   //   arr.push(this.baseUrl + a.file.thumb.url);
-  //   // });
-  //   // return arr;
-
-  //   // let arr = new Array();
-  //   // arr.push(this.baseUrl + url);
-  //   // arr.push(this.baseUrl + url);
-  //   // return arr;
-  // };
-
-  // getImageAttachment(attachments : PostAttachment[]){
-  //   return  attachments.find(a => a.type === "Image");
-  // }
-
-  // getAttachment(attachmentType: string, attachments : PostAttachment[]){
-  //     return  attachments.find(a => a.type === attachmentType);
-  // }
 }
